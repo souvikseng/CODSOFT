30691822dd1755273f44a11f961a8af05fa19957
souvikseng <152864879+souvikseng@users.noreply.github.com>
2024-12-25 15:22:56 +0530

    Add files via upload

---
+++ untitled49.py
@@ -0,0 +1,37 @@
+
+
+from sklearn import datasets
+iris = datasets.load_iris()
+
+X= iris.data # since i need the entire data so no parenthesis
+Y=iris.target   #Same here
+
+#Split the X and Y dataset into training and testing set
+from sklearn.model_selection import train_test_split
+
+X_train,X_test,Y_train,Y_test = \
+    train_test_split(X,Y, test_size=0.3, random_state=1234, stratify=Y)
+    
+    
+    
+   
+    
+   
+from sklearn.svm    import SVC
+    
+from sklearn.metrics import confusion_matrix
+   
+   
+    
+   
+    
+svc=SVC(kernel='rbf', gamma=1.0)
+  
+
+  
+svc.fit(X_train, Y_train)
+    
+Y_predict = svc.predict(X_test)
+cm_rbf= confusion_matrix(Y_test, Y_predict)
+
+#So we see that the no of correctely or true positives predicted data are more in the confusion matrix. 
